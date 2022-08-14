0ffd6f52742d4ff47ab07efb1db082bff29f87d9
wlatif <latif.wail@hotmail.com>
2022-08-14 09:50:02 +0200

    add styled components

---
+++ src/screens/styledReactNative/index.ts
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+import { styled } from "tailwindcss-react-native";
+
+const StyledText = styled(Text);
+const StyledView = styled(View);
+
+export { StyledText, StyledView };
