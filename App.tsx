06512036b0a53d52547f55336ea513d1544a26a5
wlatif <latif.wail@hotmail.com>
2022-08-14 08:58:41 +0200

    Adding react navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import Home from "./src/screens/Home";
+
+const NativeStack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <NativeStack.Navigator>
+          <NativeStack.Screen name="Home" component={Home}/>
+        </NativeStack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
